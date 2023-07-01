64b3d571833cdc00935adf92c6bd97dd50e7a1b8
luis221020931 <luis@DESKTOP-V2750M0>
2023-07-01 09:31:19 -0300

    admin page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,11 +1,84 @@
-import { Container } from "./styles"
+import React, { useState } from 'react';
+import { Container, Container0 } from './styles';
 
-const AdminPage = ()=> {
-    return(
-        <Container>
+const Formulario = ({ titulo, acao }) => {
+  const [item, setItem] = useState('');
+  const [listaItens, setListaItens] = useState([]);
+  const [mostrarLista, setMostrarLista] = useState(false);
 
-        </Container>
-    )
+  const handleChange = (event) => {
+    setItem(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setListaItens([...listaItens, item]);
+    setItem('');
+  };
+
+  const handleClickMostrarLista = () => {
+    setMostrarLista(!mostrarLista);
+  };
+
+  return (
+    <div>
+      <p>{titulo}</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={item} onChange={handleChange} />
+        <button type="submit">{acao}</button>
+      </form>
+      {listaItens.length > 0 && (
+        <>
+          <button onClick={handleClickMostrarLista}>
+            {mostrarLista ? 'Esconder Lista' : 'Mostrar Lista'}
+          </button>
+          {mostrarLista && (
+            <ul>
+              {listaItens.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+const Estatisticas = () => {
+  return (
+    <div>
+      
+      <h1>Informaçoes dos usuarios e grupos</h1>
+      
+    </div>
+    
+  )
+
 
 }
-export default AdminPage
+
+const AdminPage = () => {
+  return (
+    <div>
+      <Container>
+        <h1>Preferencias</h1>
+        <Formulario titulo="Qual você deseja adicionar?" acao="Adicionar" />
+        <Formulario titulo="Qual você deseja excluir?" acao="Excluir" />
+        <h1>Excluir usuário</h1>
+        <Formulario titulo="Qual usuário você deseja excluir?" acao="Excluir" />
+      </Container>
+      <hr></hr>
+      <Container0>
+        <h1>Qual usuario quer tirar? de qual grupo? </h1>
+        <Formulario titulo="Qual você deseja excluir?" acao="Excluir" />
+        <Formulario titulo="Qual grupo?" acao="Excluir" />
+
+      </Container0>
+
+
+    </div>
+  );
+};
+
+export default AdminPage;
--- src/pages/admin/styles.js
+++ src/pages/admin/styles.js
@@ -1,4 +1,19 @@
 import styled from "styled-components";
 export const Container = styled.div`
-`
-
+background-color: #007bff;
+padding: 20px;
+border-radius: 8px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+export const Container0 = styled.div`
+background-color: #4169e1;
+padding: 20px;
+border-radius: 8px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+export const Estatistica = styled.div`
+background-color: #87cefa;
+padding: 20px;
+border-radius: 8px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
