77ff0dec9a7a48e71d796b201b6604cf6cdefda7
luis221020931 <luisgdscat10@gmail.com>
2023-07-07 18:13:29 -0300

    Page match

--- src/pages/match/styles.js
+++ src/pages/match/styles.js
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 export const Container = styled.div`
+background-color: #ffffff;
+//overflow: hidden;
+height: 100vh;
+display: flex;
+flex-direction: column;
+font-family: var(--ff-primary);
+place-content: center;
+text-align: center;
 `
+export const HeaderContainer = styled.header`
+background-color: #29063d;
+color: #fff;
+padding: 20px;
+`;
+
+export const HeaderTitle = styled.h1`
+margin: 0;
+`;
+export const Logo = styled.h1`
+color: #FF0000;
+width: 10rem;
+height: 5.5rem;
+margin: 0;
+`;
+
+
+
