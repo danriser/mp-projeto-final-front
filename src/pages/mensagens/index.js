03c6ad8d026505b025b5f213370e0ac1866204db
luis221020931 <luisgdscat10@gmail.com>
2023-07-08 09:26:59 -0300

    Iniciando mensagens

---
+++ src/pages/mensagens/index.js
@@ -0,0 +1,13 @@
+import { Container } from "./styles"
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
+
+const Mensagens = ()=> {
+    return(
+        <Container>
+
+        </Container>
+    )
+
+}
+export default Mensagens
