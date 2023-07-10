75f1b9ca50d777e4a6743cba3c1541c18b02910b
dan <danieljosepalmar@gmail.com>
2023-07-10 12:14:12 -0300

    home inicio

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,39 @@
 import { Container } from "./styles"
 
 const Home = ()=> {
+
+    function openMenu() {
+        document.getElementById("myMenu").style.width = "250px"
+    }
+    
+    function closeMenu() {
+        document.getElementById("myMenu").style.width = "0"
+    }
     return(
         <Container>
+            <body>
+
+                {/* <div id="myMenu" className="menu">
+                    <a href="javascript:void(0)" className="close" onclick= {closeMenu()}> &times;</a>
+                    <a href="#">Mensagens</a>
+                    <a href="#">Match</a>
+                    <a href="#">Grupos</a>
+                </div> */}
+
+                {/* <span style = {{fontsize:'30px', cursor:'pointer'}} onclick={openMenu()}>&#9776;</span>  */}
+                <div className="profile"><a href="#" style={{color: "clrwhite"}}>Perfil</a></div>
+                <div className="logo"><img src="logoooo.png.png"></img></div>
+
+                <div className="container">
+                    <div className="box">Grupos de Músicas</div>
+                    <div className="box">Grupos de Jogos</div>
+                    <div className="box">Grupos de Filmes</div>
+                    <div className="box">Grupos de Séries</div>
+                </div>
+
+
+
+            </body>
 
         </Container>
     )
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,3 +1,115 @@
 import styled from "styled-components";
 export const Container = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+:root {
+    color: #06022c;
+    background-color: #21082c;
+    --clr-white: #fff;
+    --clr-gray: #d9d9d9;
+    --clr-dark-gray: #71767b;
+
+    font-family: 'Inter', sans-serif;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font: inherit;
+}
+
+profile{
+    color:white;
+}
+
+body {
+    font-family: var(--ff-primary);
+    background-color: var(--clr-background);
+    color: var(--clr-white);
+}
+
+#myMenu {
+    background-color: var(--clr-header);
+    font-size: 1.2em;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 0;
+    transition: 0.5s;
+    z-index: 2;
+    overflow-x: hidden;
+    padding-top: 60px;
+}
+
+.menu a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    color: var(--clr-white);
+    display: block;
+    transition: 0.3s;
+}
+
+.menu .close {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+    color: var(--clr-white);
+}
+
+.container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 100px;
+    gap: 25px;
+    margin-top: 100px;
+}
+
+.box {
+    width: 200px;
+    height: 200px;
+    background-color: var(--dark-gray);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+    cursor: pointer;
+    border-radius: 1rem;
+    font-size: 1.2em;
+    font-weight: 700;
+    transition: 0.3s;
+}
+
+.box:hover {
+    transform: scale(1.05);
+}
+
+#profile {
+    position: fixed;
+    top: 0;
+    right: 0;
+    padding: 10px;
+    font-size: 1.2em;
+    z-index: 1;
+}
+
+#logo {
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 0px;
+    z-index: 1;
+}
+
+#logo img {
+    height: 150px;
+}
+
 `
